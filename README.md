6978b2fa36e54c11cc778457d053deec8740b68e
Victor (bye) <victorrodrigues098890@gmail.com>
2025-08-19 05:44:09 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Testador mc102
+## Descrição
+Este projeto que fiz é baseado no testador já disponível no [site](https://ic.unicamp.br/~mc102/) da matéria. A diferença é que o fiz completamente em Bash, o que significa que ele pode ser facilmente usado em sistemas baseados em Linux e, com algumas alterações, também pode ser usado no macOS.
+
+## Considerações
+Esta é a primeira vez que faço um programa em Bash e realmente me diverti mexendo com essa linguagem. Apesar de me perder um pouco com a quantidade de notações, achei muito legal a facilidade de manipular arquivos com ela, pretendo usar em projetos futuros.
---
+++ testador.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+shopt -s nullglob
+
+echo "Bem-vindo ao testador.sh!"
+
+pyfiles=( lab*.py )
+arqpy=${pyfiles[0]}
+arqin=( *.in )
+arqout=( *.out )
+
+if [ -z "$arqpy" -o ${#arqin[@]} -eq 0 -o ${#arqout[@]} -eq 0 ]; then
+	echo "Erro: algum dos arquivos necessários não foi encontrado no diretório atual"
+	exit 1
+fi
+
+for i in "${!arqin[@]}"; do
+	entrada="${arqin[i]}"
+	saida_lgl="${arqout[i]}"
+	
+	saida=$(python3 "$arqpy" < "$entrada")
+	
+	saida_sem_espaco=$(echo "$saida" | sed 's/[[:space:]]*$//')
+	esperado=$(cat "$saida_lgl" | sed 's/[[:space:]]*$//')
+	
+	
+	if [[ "$saida_sem_espaco" == "$esperado" ]]; then
+		echo " "
+    		echo -e "\e[32m$entrada: ok \e[0m"
+	else
+		echo " "
+    		echo -e "\e[31m$entrada: falhou \e[0m"
+    		echo " "
+    		echo "Saída esperada:"
+    		echo "$esperado"
+    		echo " "
+    		echo "Saída registrada:"
+    		echo "$saida_sem_espaco"
+	fi
+done
+
+echo " "
+echo "Fim do programa"
+
+exit 0
